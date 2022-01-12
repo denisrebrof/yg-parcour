3a546464e33e95330f86e1031e112b38ec55a71c
Denis Rebrov <denisrebrof@gmail.com>
2022-01-12 22:00:16 +0300

    added start session event, try fix input, added ad on start

--- Assets/Scripts/Analytics/GameAnalyticsInitializer.cs
+++ Assets/Scripts/Analytics/GameAnalyticsInitializer.cs
@@ -6,5 +6,6 @@ public class GameAnalyticsInitializer : MonoBehaviour
     void Awake()
     {
         GameAnalytics.Initialize();
+        GameAnalytics.NewDesignEvent("Game Session Started");
     }
 }
