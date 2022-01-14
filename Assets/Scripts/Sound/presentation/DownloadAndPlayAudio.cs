6a3a1b20840c4cc1a62dc64eb661a33570dad8e5
Denis Rebrov <denisrebrof@gmail.com>
2022-01-14 19:31:21 +0300

    setting up mobile input and audio

---
+++ Assets/Scripts/Sound/presentation/DownloadAndPlayAudio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Sound.presentation
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class DownloadAndPlayAudio: MonoBehaviour
+    {
+        public string url;
+        public AudioSource source;
+
+        IEnumerator Start()
+        {
+            source = GetComponent<AudioSource>();
+            using var www = new WWW(url);
+            yield return www;
+            try
+            {
+                source.clip = www.GetAudioClip();
+                source.Play();
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+            }
+            
+        }
+    }
+}
