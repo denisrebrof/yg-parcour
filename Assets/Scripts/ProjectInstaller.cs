e7e3544d73d0920b91bc468ac28c41eaa97e32ef
Denis Rebrov <denisrebrof@gmail.com>
2022-01-19 18:37:41 +0300

    try fix input handler

--- Assets/Scripts/ProjectInstaller.cs
+++ Assets/Scripts/ProjectInstaller.cs
@@ -9,7 +9,8 @@ public class ProjectInstaller : MonoInstaller
     public override void InstallBindings()
     {
         Container.BindInstance(look).AsSingle();
-        Container.BindInterfacesAndSelfTo<InputHandler>().FromNew().AsSingle();
+        var handler = new InputHandler();
+        Container.BindInterfacesAndSelfTo<InputHandler>().FromInstance(handler).AsSingle();
         Container.Bind<FirstPersonLook.ILookDeltaProvider>().To<FirstPersonLookDeltaProviderRouter>().AsSingle();
         Container.Bind<FirstPersonMovement.IMovementInputProvider>().To<MovementInputProviderRouter>().AsSingle();
         Container.Bind<Jump.IJumpInputProvider>().To<JumpInputProviderRouter>().AsSingle();
