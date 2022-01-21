66efa9cab3c4f4c12471421d9e4e74f31945bd84
Denis Rebrov <denisrebrof@gmail.com>
2022-01-21 19:58:09 +0300

    added platform switcher

---
+++ Assets/Scripts/SDK/Editor/SwitchSDKPlatform.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+
+namespace SDK.Editor
+{
+    public static class SwitchSDKPlatform
+    {
+        private static readonly BuildTargetGroup targetGroup = BuildTargetGroup.WebGL;
+
+        private static readonly Dictionary<SDKProvider.SDKType, string> Symbols =
+            new Dictionary<SDKProvider.SDKType, string>()
+            {
+                {SDKProvider.SDKType.Yandex, "YANDEX_SDK"},
+                {SDKProvider.SDKType.Vk, "VK_SDK"},
+                {SDKProvider.SDKType.Poki, "POKI_SDK"},
+                {SDKProvider.SDKType.None, ""}
+            };
+
+        [MenuItem("Platform/Set Yandex", false)]
+        public static void SetYandex()
+        {
+            CleanDefineSymbols();
+            SetPlatformDefineSymbol(SDKProvider.SDKType.Yandex);
+        }
+
+        [MenuItem("Platform/Set VK", false)]
+        public static void SetVk()
+        {
+            CleanDefineSymbols();
+            SetPlatformDefineSymbol(SDKProvider.SDKType.Vk);
+        }
+
+        [MenuItem("Platform/Set None", false)]
+        public static void SetNone()
+        {
+            CleanDefineSymbols();
+        }
+
+        private static void CleanDefineSymbols()
+        {
+            var currData = PlayerSettings.GetScriptingDefineSymbolsForGroup(targetGroup);
+            var definitions = currData.Split(';').ToList();
+            var sdkDefinitions = Symbols.Values.ToList();
+            definitions = definitions.Where(definition => !sdkDefinitions.Contains(definition)).ToList();
+            var cleanedData = String.Join(';', definitions);
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(targetGroup, cleanedData);
+        }
+
+        private static void SetPlatformDefineSymbol(SDKProvider.SDKType type)
+        {
+            string currData = PlayerSettings.GetScriptingDefineSymbolsForGroup(targetGroup);
+            if (currData.Length > 0 && !currData[^1].Equals(';'))
+            {
+                currData += ';';
+            }
+            currData += Symbols[type];
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(targetGroup, currData);
+        }
+    }
+}
