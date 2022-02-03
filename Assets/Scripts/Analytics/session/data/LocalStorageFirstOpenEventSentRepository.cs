e567ea7365fbb365cb62db87e607f50655981f68
Denis Rebrov <denisrebrof@gmail.com>
2022-02-03 17:47:18 +0300

    crazy happytime fix

--- Assets/Scripts/Analytics/session/data/LocalStorageFirstOpenEventSentRepository.cs
+++ Assets/Scripts/Analytics/session/data/LocalStorageFirstOpenEventSentRepository.cs
@@ -8,7 +8,7 @@ namespace Analytics.session.data
     public class LocalStorageFirstOpenEventSentRepository: IFirstOpenEventSentRepository
     {
         private const string PrefsKeyPrefix = "FirstOpenEventSent";
-        public bool IsFirstOpen() => !LocalStorageIO.HasKey(PrefsKeyPrefix);
+        public bool IsFirstOpen() => !LocalStorageIO.HasKey(PrefsKeyPrefix); 
         public void SetFirstOpenAppeared() => LocalStorageIO.SetString(PrefsKeyPrefix, "true");
     }
 }
